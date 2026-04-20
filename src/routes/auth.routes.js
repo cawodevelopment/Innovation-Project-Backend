078972c60d67217cd302d678c07654ba18c501fd
cawodevelopment <cawodevelopment@outlook.com>
2026-04-20 12:07:18 +0100

    disable register route for demo security

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,7 @@ import authenticate from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/register', authController.registerUser);
+// router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 router.post('/refresh', authController.refreshToken);
 router.post('/logout', authenticate, authController.logoutUser);
